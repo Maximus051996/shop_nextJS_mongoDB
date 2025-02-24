d78a46b56b1363a5667a235969f541f5f356531c
sayan pramanick <sayanp05@gmail.com>
2025-02-24 19:49:34 +0530

    commit code for ESLint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     return (
         <AuthProvider>
             <html lang="en">
-                <body className={`${inter.className} highlighter-context`}>
+                <body className={inter.className}>
                     <main>{children}</main>
                 </body>
             </html>
